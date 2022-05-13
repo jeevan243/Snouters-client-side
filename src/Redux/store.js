8f8ecea7c2dad0f631141ba4a308152613ac3133
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-05-13 13:13:06 +0530

    thunk updated

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -9,9 +9,9 @@ const roodReducer = combineReducers({
     petData: petDataReducer
 })
 
-// const thunk = (store) => (next) => (action) => {
-//     if (typeof action === "function") return action(store.dispatch)
-//     next(action);
-// }
+const thunk = (store) => (next) => (action) => {
+    if (typeof action === "function") return action(store.dispatch)
+    next(action);
+}
 
 export const store = legacy_createStore(roodReducer, applyMiddleware(thunk))
